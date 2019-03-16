c7b4b1af02e01dc522f49dac8a363465a029c556
ludzik940 <djurczak94@gmail.com>
2019-03-16 19:25:36 +0100

    update for live

--- app.js
+++ app.js
@@ -14,3 +14,33 @@ document.getElementById('tabList').addEventListener('click', function(e){
   });
 
 
+
+$(document).ready(function(){
+
+  $('.mailContact').on('mouseenter', function(){
+      $('#mailP').css('visibility', 'visible');
+      $('#mailP').slideDown();
+  }).on('mouseleave', function(){
+    $('#mailP').css('visibility', 'hidden');
+      $('#mailP').slideUp();
+      
+  });
+
+  $('.phoneContact').on('mouseenter', function(){
+    $('#phoneP').css('visibility', 'visible');
+    $('#phoneP').slideDown();
+}).on('mouseleave', function(){
+  $('#phoneP').css('visibility', 'hidden');
+    $('#phoneP').slideUp();
+});
+
+$('.facebookContact').on('mouseenter', function(){
+  $('#facebookP').css('visibility', 'visible');
+  $('#facebookP').slideDown();;
+}).on('mouseleave', function(){
+  $('#facebookP').slideUp();
+  $('#facebookP').css('visibility', 'hidden');
+});
+
+
+});
